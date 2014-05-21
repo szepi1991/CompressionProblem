4e17c1f1e8968647a854941327b1b86282256aca
David Szepesvari <szepesvari.david@gmail.com>
2014-05-21 04:55:19 -0400

    We do comparision based on lower bound.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,9 +1,118 @@
+import java.math.BigInteger;
 
 public class Calculator {
 
+	private static final boolean PRINT_INFO = true;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// try for some values of n
+		System.out.println("Trying for some values of n, with the lower bound"
+				+ " (rather than exact calculation)");
+		System.out.println("I display |V|*VCdim - |E|. We are happy if it's negative");
 
+		for (int n = 2; n < 50; n += 3) {
+			System.out.print(2*n + " - ");
+			cannotCompress(n, true);
+		}
+	}
+	
+	/**
+	 * This function returns whether we have proof that there is no compression.
+	 * 
+	 * @param n	HALF the number of numbers we are considering total orders on
+	 * @param approx if true, we use the lower bound on the number of edges
+	 * @return true iff we proved there is no compression, ie |V|*VCdim < |E|
+	 */
+	public static boolean cannotCompress(int n, boolean approx) {
+		if (approx == false) 
+			throw new RuntimeException("This is not implemented yet!");
+		
+		BigInteger numEdges = getNumEdgesL(n);
+		BigInteger numVerts = getNumVertices(n);
+		BigInteger diff = numVerts.multiply(BigInteger.valueOf(n-1)).subtract(numEdges);
+		boolean proved = (diff.signum() == -1);
+		
+		if (PRINT_INFO) {
+			System.out.print(proved ? "YES!" : "nope");
+			System.out.print(": " + diff);
+//			System.out.print(": " + numVerts + "*" + (n-1));
+//			System.out.print(proved ? ">=" : "<");
+//			System.out.print(numEdges);
+			System.out.println();			
+		}
+		
+		return proved;
+	}
+	
+	/**
+	 * This function returns (a lower bound on) the number of edges in the one 
+	 * inclusion graph of the concept classes to the sample we talked about.
+	 * 
+	 * @param n	HALF the number of numbers we are considering total orders on
+	 */
+	public static BigInteger getNumEdgesL(int n) {
+		BigInteger eLower = new BigInteger("0");
+		for (int k = 1; k < n; ++k) {
+			// this corresponds to one summand in the sum
+			BigInteger prod = factorial(k).pow(2);
+			prod = prod.multiply(BigInteger.valueOf(4*k+1));
+			prod = prod.multiply(Stirling.stirling(n-1, k));
+			prod = prod.multiply(Stirling.stirling(n, k+1));
+			eLower = eLower.add(prod);
+		}
+		
+		return eLower.multiply(BigInteger.valueOf(n*n)); 
+	}
+
+//	/**
+//	 * This function returns the number of edges in the one inclusion graph
+//	 * of the concept classes to the sample we talked about.
+//	 * 
+//	 * @param n	HALF the number of numbers we are considering total orders on
+//	 */
+//	public static BigInteger getNumEdges(int n) {
+//		BigInteger eLower = new BigInteger("0");
+//		for (int k = 1; k < n; ++k) {
+//			// this corresponds to one summand in the sum
+//		}
+//		
+//		return eLower.multiply(BigInteger.valueOf(n*n)); 
+//	}
+	
+	
+	/**
+	 * This function returns the number of vertices in the one inclusion graph
+	 * of the concept classes to the sample we talked about.
+	 * 
+	 * @param n	HALF the number of numbers we are considering total orders on
+	 */
+	public static BigInteger getNumVertices(int n) {
+		BigInteger verts = new BigInteger("0");
+		for (int k = 1; k <= n; ++k) {
+			// this corresponds to one summand in the sum
+			BigInteger prod = factorial(k).pow(2);
+			prod = prod.multiply(Stirling.stirling(n, k));
+			prod = prod.multiply(Stirling.stirling(n+1, k+1));
+			verts = verts.add(prod);
+		}
+		
+		return verts; 
+	}
+	
+	
+	
+	/**
+	 * The standard factorial function.
+	 */
+	public static BigInteger factorial(int n) {
+		// n is probably not so big, so it's ok not to cache.
+		// though we construct a lot of BigIntegers... TODO ?
+		if (n == 0) return BigInteger.valueOf(1);
+		BigInteger ret = BigInteger.valueOf(n);
+		while (--n > 0) {
+			ret = ret.multiply(BigInteger.valueOf(n));
+		}
+		return ret;
 	}
 	
 }
