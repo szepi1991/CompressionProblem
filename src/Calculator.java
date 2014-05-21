ca40819f39cab0cd365d3007d5c08d35a304bfcf
David Szepesvari <szepesvari.david@gmail.com>
2014-05-21 02:50:21 -0400

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,18 @@
+import java.math.BigInteger;
+
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public BigInteger stringling(int n, int k) {
+		// we calculate it recursively.. 
+		// filling the table bottom-up would probably be better
+		
+		return null; // TODO temp
+	}
+
+}
